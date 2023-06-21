e628611818dcb414a7b2bdc01e6b30279b6c1d27
Stefan Imig <imigstefan@gmail.com>
2023-06-21 14:15:33 +0200

    fixed pyodide module caching

--- frontend-svelte/src/routes/challenge/[slug]/+page.svelte
+++ frontend-svelte/src/routes/challenge/[slug]/+page.svelte
@@ -1,13 +1,13 @@
 <script>
     // import { loadPyodide } from "pyodide";
     import { onMount } from "svelte";
+    import {XMLParser} from 'fast-xml-parser'
+
+    const parser = new XMLParser();
     /** @type {import('./$types').PageData} */
     export let data;
-
-    console.log(data);
-
-
     let pyodide;
+    let userCode = data.template;
 
     onMount(async ()=>{
         pyodide = await loadPyodide()
@@ -16,44 +16,25 @@
         const micropip = pyodide.pyimport("micropip");
         await micropip.install('hypothesis');
         await micropip.install('pytest')
-        await micropip.install('pytest-xdist')
 
-        // pyodide.FS.writeFile("test_.py", data.test, {encoding: "utf8"})
-
-        // extract the folder
-        // const response = await fetch(data.archivePath);
-        // let buffer = await response.arrayBuffer();
-        // await pyodide.unpackArchive(buffer, 'zip');
-        // pyodide.pyimport("your_package");
-
-        pyodide.runPython(`import os
-print(os.listdir())`)
-
+        pyodide.FS.writeFile("test_.py", data.test, {encoding: "utf8"})
+        pyodide.FS.writeFile("solution.py", userCode, {encoding: "utf8"})
     })
 
-
-    let userCode = data.template
-
     async function testUserCode(){
-        // console.log(userCode);
-
-
         pyodide.FS.writeFile("solution.py", userCode, {encoding: "utf8"})
-        pyodide.FS.writeFile("test_.py", data.test, {encoding: "utf8"})
+        let exitCode = pyodide.runPython(`from importlib import reload
+import pytest
+import test_
+import solution
 
-//         pyodide.runPythonAsync(`
-// with open("solution.py") as f:
-//     print(f.readlines())`)
+reload(solution)
+reload(test_)
 
-        pyodide.code.eval(`import pytest
-pytest.main()`)
-        // let pkg = pyodide.pyimport("test_");
-        // let x = pkg.test();
-        // console.log(x);
-
-        // pyodide.runPython(data.test)
+pytest.main(['--junitxml', 'report.xml'])`)
+        console.log("exitcode", exitCode);
+        let report = pyodide.FS.readFile('report.xml',  { encoding: "utf8" })
     }
-    
 </script>
 
 <div>
