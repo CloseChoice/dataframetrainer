5f15a0f05c314ba7c388fed4deb2632bb8343aeb
Tobias Pitters <tobias.pitters@gmail.com>
2023-07-13 16:09:44 +0200

    add user challenges table

--- database/sql/challenges.sql
+++ database/sql/challenges.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS challenges(
-    id varchar(100) NOT NULL
+    id TEXT NOT NULL,
+
+    CONSTRAINT "Challenges_pkey" PRIMARY KEY ("id")
 ) TABLESPACE pg_default;
 
 COPY challenges FROM PROGRAM 'ls /challenges/ | grep "^[[:upper:]]"';
---
+++ database/sql/user_challenges.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "user_challenges" (
+    "UserId" TEXT NOT NULL,
+    "ChallengeId" TEXT,
+    "SessionId" TEXT,
+    "Successful" BOOLEAN,
+    "Score" FLOAT,
+
+    CONSTRAINT "User_fkey" FOREIGN KEY ("UserId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Challenge_fkey" FOREIGN KEY ("ChallengeId") REFERENCES "challenges"("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
