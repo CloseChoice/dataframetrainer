9041496ca6b201cdebf6970e32bd74b3df204775
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2023-07-20 12:41:18 +0200

    comment out inserts (might interfere with auth js)

--- database/sql/user_challenges.sql
+++ database/sql/user_challenges.sql
@@ -1,11 +1,11 @@
-CREATE TABLE "user_challenges" (
-    "UserId" TEXT NOT NULL,
-    "ChallengeId" TEXT,
-    "SessionId" TEXT,
-    "Timestamp" TIMESTAMP NOT NULL,
-    "Successful" BOOLEAN,
-    "Score" FLOAT,
+-- CREATE TABLE "user_challenges" (
+--     "UserId" TEXT NOT NULL,
+--     "ChallengeId" TEXT,
+--     "SessionId" TEXT,
+--     "Timestamp" TIMESTAMP NOT NULL,
+--     "Successful" BOOLEAN,
+--     "Score" FLOAT,
 
-    CONSTRAINT "User_fkey" FOREIGN KEY ("UserId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "Challenge_fkey" FOREIGN KEY ("ChallengeId") REFERENCES "challenges"("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
+--     CONSTRAINT "User_fkey" FOREIGN KEY ("UserId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+--     CONSTRAINT "Challenge_fkey" FOREIGN KEY ("ChallengeId") REFERENCES "challenges"("id") ON DELETE CASCADE ON UPDATE CASCADE
+-- );
