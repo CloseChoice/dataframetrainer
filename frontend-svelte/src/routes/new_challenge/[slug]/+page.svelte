c66e6add2fd0d364c05c8f07ffbdd85d618bd69d
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2023-07-21 14:55:58 +0200

    added resizing functionality

--- frontend-svelte/src/routes/new_challenge/[slug]/+page.svelte
+++ frontend-svelte/src/routes/new_challenge/[slug]/+page.svelte
@@ -3,8 +3,11 @@
     import CodeOutput from "./CodeOutput.svelte";
     /** @type {import('./$types').PageData} */
     export let data;
+    // https://github.com/nathancahill/split/tree/master/packages/splitjs
+    import Split from 'split.js'
 
     import {executeUserCode, testUserCode, isPyodideReady} from './python-runner'
+    import { onMount } from "svelte";
 
     const descritption = data.intro
     let code = data.default_code
@@ -16,16 +19,36 @@
     async function handleTest(){
         testUserCode(code, data)
     }
+
+    let splitEditor;
+    let splitConsole;
+    let splitLeft;
+    let splitRight;
+    onMount(()=>{
+        Split([splitEditor, splitConsole], {
+            sizes: [75, 25],
+            minSize: [200, 200],
+            direction: 'vertical',
+            snapOffset: 0,
+        })
+
+        Split([splitLeft, splitRight], {
+            sizes: [50, 50],
+            minSize: [200, 200],
+            snapOffset: 0,
+        })
+    })
+
 </script>
 
 <div class="h-100">
     <div class="row gx-0 h-100">
-        <div class="col-6">
+        <div bind:this={splitLeft} class="col-6">
             {@html descritption}
         </div>
-        <div class="h-100 col-6 d-flex flex-column" >
+        <div bind:this={splitRight} class="h-100 col-6 d-flex flex-column" >
             <!-- min height:0 is necessary to prevent overflow  -->
-            <div class="flex-grow-1 position-relative" style="min-height:0">
+            <div bind:this={splitEditor} class="flex-grow-1 position-relative" style="min-height:0">
 
                     <CodeMirror bind:value={code}/>
                     <div class="text-light top-0 end-0 position-absolute d-flex justify-content-end gap-2 p-2">
@@ -37,10 +60,22 @@
                         <button disabled='{!$isPyodideReady}' type="button" on:click={handleTest} class="btn btn-secondary btn-sm">Test</button>
                     </div>
             </div>
-            <div style="min-height: 200px; background: green" >
+            <div bind:this={splitConsole} style="min-height: 200px; background: green" >
                 shish
                 <!-- <CodeOutput resultUserCode={resultUserCode}/> -->
             </div>
         </div>
     </div>
 </div>
+
+
+<style>
+    :global(.gutter){
+
+        background-color: black;
+    }
+    :global(.gutter:hover){
+
+        background-color: blue;
+    }
+</style>
