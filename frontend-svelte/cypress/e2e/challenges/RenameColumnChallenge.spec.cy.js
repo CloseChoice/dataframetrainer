83fc40f8463cff18bcf50c3e1430933f276cdff1
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2023-08-03 01:20:46 +0200

    use cypress baseurl

--- frontend-svelte/cypress/e2e/challenges/RenameColumnChallenge.spec.cy.js
+++ frontend-svelte/cypress/e2e/challenges/RenameColumnChallenge.spec.cy.js
@@ -1,7 +1,7 @@
-const baseUrl = 'http://localhost:5173/new_challenge/RenameColumn';
+
 describe('RenameColumn Test', () => {
     it('Confirm pandas import in CodeEditor', () => {
-        cy.visit(baseUrl)
+        cy.visit('/new_challenge/RenameColumn')
         cy.get('.cm-line').should('contain', 'import pandas as pd');
     })
   })
