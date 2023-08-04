be804c9bef7f053ebdb433c33b842ce99b07083f
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2023-08-04 13:12:30 +0200

    added form validation

--- frontend-svelte/src/routes/authentication/+page.svelte
+++ frontend-svelte/src/routes/authentication/+page.svelte
@@ -1,11 +1,35 @@
+<script>
+  /** @type {import('./$types').PageData} */    export let data;
+  /** @type {import('./$types').ActionData} */    export let form;
+
+
+  
+</script>
+
 <form method="POST" novalidate>
   <div class="mb-3">
     <label for="nameInput" class="form-label" >User Name</label>
-    <input data-test="name-input" required name="name" type="text" class="form-control" id="nameInput" aria-describedby="nameHelp">
+    <input 
+      class:is-invalid={form?.username?.isValid === false} 
+      class:is-valid={form?.username?.isValid === true} 
+      data-test="name-input" required name="name" type="text" class="form-control" id="nameInput" aria-describedby="nameHelp">
+    <div class="invalid-feedback" >
+      {form?.username?.feedback}
+    </div>
+    <div>
+
+    </div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input data-test="password-input" required name="password" type="password" class="form-control" id="exampleInputPassword1">
+    <input 
+    class:is-invalid={form?.password?.isValid === false} 
+    class:is-valid={form?.password?.isValid === true} 
+    data-test="password-input" required name="password" type="password" class="form-control" id="exampleInputPassword1">
+    <div class="invalid-feedback">
+      {form?.password?.feedback}
+    </div>
+  
   </div>
 
   <button data-test="register-button" formaction="?/register" type="submit" class="btn btn-primary">Register</button>
