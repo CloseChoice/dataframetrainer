aab656dea807f32e83c4fe78ba20ebfb39bb051b
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2023-08-14 18:43:51 +0200

    change layout

---
+++ frontend-svelte/src/routes/(fullscreen)/new_challenge/[slug]/CodeMirror.svelte
@@ -0,0 +1,62 @@
+<script>
+    import {EditorState} from "@codemirror/state"
+    import {EditorView, keymap} from "@codemirror/view"
+    import {defaultKeymap} from "@codemirror/commands"
+    import { onMount } from 'svelte';
+    import { oneDark } from "@codemirror/theme-one-dark"
+    import { history } from "@codemirror/commands";
+    import { highlightSpecialChars } from "@codemirror/view";
+    import { drawSelection } from "@codemirror/view";
+    import { indentOnInput } from "@codemirror/language";
+    import { autocompletion, closeBrackets } from '@codemirror/autocomplete';
+    import { highlightSelectionMatches } from "@codemirror/search";
+    import { python } from "@codemirror/lang-python";
+
+    let codeMirrorRef
+
+    export let value = ""
+
+    const extensions = [
+        keymap.of(defaultKeymap),
+        // Listen to changes of the code editor and update value
+        EditorView.updateListener.of(function(e) {
+            value = e.state.doc.toString();
+        }),
+        oneDark,
+        history(),
+        highlightSpecialChars(),
+      drawSelection(),
+      EditorState.allowMultipleSelections.of(true),
+      indentOnInput(),
+      closeBrackets(),
+      autocompletion(),
+      highlightSelectionMatches(),
+      python()
+    ]
+
+    onMount(()=>{
+        let editorState = EditorState.create({
+            doc: value,
+            extensions: extensions
+        })
+
+        let editorView = new EditorView({
+            state: editorState,
+            parent: codeMirrorRef
+        })
+    })
+
+</script>
+
+
+<div class="CodeMirror" bind:this={codeMirrorRef}></div>
+
+<style>
+    .CodeMirror {
+        height: 100%;
+        max-height: 100%;
+    }
+    :global(.cm-editor){
+        height: 100%;
+    }
+</style>
---
+++ frontend-svelte/src/routes/(with_footer)/+page.svelte
@@ -0,0 +1,8 @@
+<div class="container">
+<h1>Welcome</h1>
+This is the Dataframetrainer Landing page
+
+<p>
+d
+</p>
+</div>
