60122ab0a5492d75b35dfd1782b97d89f62d770c
Tobias Pitters <tobias.pitters@gmail.com>
2023-08-30 15:49:29 +0200

    WIP: add setup to cypress test

--- frontend-svelte/cypress/e2e/challenges/RenameColumnChallenge.spec.cy.js
+++ frontend-svelte/cypress/e2e/challenges/RenameColumnChallenge.spec.cy.js
@@ -1,6 +1,8 @@
 
 describe('RenameColumn Test', () => {
+
     it('Confirm pandas import in CodeEditor', () => {
+        cy.setup()
         cy.visit('/new_challenge/RenameColumn')
         cy.get('.cm-line').should('contain', 'import pandas as pd');
     })
