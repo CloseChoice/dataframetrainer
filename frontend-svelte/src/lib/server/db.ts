adc17c019f8d56f508b231b3e5dae6fb97657457
Tobias Pitters <tobias.pitters@gmail.com>
2023-09-10 20:22:54 +0200

    add log statements

--- frontend-svelte/src/lib/server/db.ts
+++ frontend-svelte/src/lib/server/db.ts
@@ -4,7 +4,7 @@ import pg from 'pg'
 import {DB_NAME, DB_USER, PASSWORD, DB_PORT} from '$env/static/private'
 
 if (!(DB_NAME && DB_USER && PASSWORD && DB_PORT)){
-  throw new Error("Not all environment variables are defined")
+  throw new Error(`Not all environment variables are defined. DB_NAME = ${DB_NAME}, DB_USER = ${DB_USER}, PASSWORD = ${PASSWORD}, DB_PORT = ${DB_PORT}`)
 }
 export const PG_CONNECTION_STRING = `postgres://${DB_USER}:${PASSWORD}@db:${DB_PORT}/${DB_NAME}`
 
