7f543d14c77b6055de7fe4990ac6364198e74917
Tobias Pitters <tobias.pitters@gmail.com>
2023-09-10 20:05:47 +0200

    update e2e tests and .example.env for prod tests

--- frontend-svelte/svelte.config.js
+++ frontend-svelte/svelte.config.js
@@ -10,11 +10,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		// todo: we should not do this in production, check how we could fix this at some other point
-		csrf: {
-			checkOrigin: false,
-		}
+		adapter: adapter()
 	}
 };
 
