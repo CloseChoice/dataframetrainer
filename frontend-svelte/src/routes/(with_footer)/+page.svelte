79698703143456a87f2efad0a35cc23c847fb99f
Tobias Pitters <tobias.pitters@gmail.com>
2023-09-12 17:33:17 +0200

    remove schmage page and add landing page text

--- frontend-svelte/src/routes/(with_footer)/+page.svelte
+++ frontend-svelte/src/routes/(with_footer)/+page.svelte
@@ -1,8 +1,35 @@
 <div class="container">
-<h1>Welcome</h1>
-This is the Dataframetrainer Landing page
+<br>
+<br>
+<h3>Welcome to Dataframetrainer</h3>
+We are a website dedicated to help you improve your knowledge of the Python data science stack
+entirely in the browser. You do not need to install anything, just register (or don't) and start learning.
+We provide a variety of challenges and can help guide you through them if you are logged in 
+BUT we never require you to do anything, you can just browse the challenges and solve them in whatever order you like.
+<br>
+<br>
+<h4>What is a Challenge?</h4>
+A challenge is a task that you have to solve using Python.
+Tasks are usually transformations on pandas Datatrame or numpy arrays.
+We have a range of challenges from easy to hard, but since this depends from person to person
+we track the individual skill level of registered users and provide them with challenges
+that are suitable for them.
+<br>
+<br>
+<h4>How do I solve a Challenge?</h4>
+Just have a look at the <i>Browse</i> bar at the top and click a challenge,
+then you will be presented with a description of the challenge and a code editor.
+The code editor is pre-filled with a template that you can use to solve the challenge.
+You can run the code by clicking the <i>Run</i> button.
+If you are confident that your solution is correct, click the <i>Test</i> button and wait a couple of seconds 
+while we run a bunch of tests on your solution. If all tests pass, you solved the challenge,
+if not try again or move on to another one.
+<br>
+<br>
+<h4>Privacy Considerations</h4>
+The site can be used without signing up. However we can provide a personalized learning experience only to 
+signed in users. Signing up does not require any personal information, just a username and a password. Since we save
+the passwords in a hashed format we cannot help you if you forget your password.
+For logged in users we save every challenge they try and solve in order to improve our recommendation.
 
-<p>
-d
-</p>
 </div>
