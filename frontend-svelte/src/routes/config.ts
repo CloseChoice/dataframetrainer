7b62578e167c6281eacb2300fca4941ee4aa0b23
Tobias Pitters <31857876+CloseChoice@users.noreply.github.com>
2023-09-15 19:15:40 +0200

    Feature/fix status of challenges (#130)
    * update routes for dev and prod builds
    
    * update config
    
    * add http to flask server
    
    * update servers

--- frontend-svelte/src/routes/config.ts
+++ frontend-svelte/src/routes/config.ts
@@ -1,3 +1,4 @@
 export const API_BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'http://www.dataframetrainer.com:5000'
-  : 'http://localhost:5000';
+  // we use nginx as reverse proxy, so it redirects all requests to http://www.dataframetrainer.com:5000 to localhost:4999
+  ? 'https://www.dataframetrainer.com:5000'
+  : 'http://localhost:4999';
