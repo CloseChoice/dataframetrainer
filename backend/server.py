32e593b0dc20b80b36517694096d044cf767de41
Tobias Pitters <tobias.pitters@gmail.com>
2023-09-15 18:34:14 +0200

    add http to flask server

--- backend/server.py
+++ backend/server.py
@@ -9,6 +9,7 @@ import json
 import numpy as np
 import pandas as pd
 from flask_cors import CORS, cross_origin
+import os
 
 from hypothesis.extra.pandas import data_frames, column, range_indexes, series
 from hypothesis.extra.numpy import arrays
@@ -39,11 +40,6 @@ DEFAULT_GROUP = 'elo_group'
 #     'elo_group': 0.9,
 #     'other_group': 0.1
 # }
-
-@app.route("/login")
-@cross_origin(supports_credentials=True)
-def login():
-    return jsonify({"success": "ok"})
 
 
 def check_ping(hostname):
@@ -243,45 +239,15 @@ def post_next_challenge():
         }
     )
 
-
-@app.route("/challenges/<int:id>/", methods=["GET"])
-@cross_origin(supports_credentials=True)
-def hello(id):
-    cursor.execute(f"select * from challenges where id = {id}")
-    result = cursor.fetchall()
-    # evaluate the string expression to be able to call .example()
-    # .example creates an example from the dataframe
-    print(result)
-    exec(result[0][1])
-    df = locals()["initial"]()
-    # in result[0][2] there is a function definition like
-    # def transform:
-    #     df[1] = df[1] + 2
-    #     return df
-    # this definition will be called with `exec`
-    print("\n\n")
-    print("This is result[0][2]")
-    print(f"{result[0][2]}")
-    print(
-        f"Try to get locals().get('transform', 'not found'): {locals().get('transform', 'not found')}"
-    )
-    exec(result[0][2])
-    # transform is now defined and acted upon df
-    exec(result[0][3])
-    exec(result[0][4])
-    df_static = locals()["static_example"]()
-    df_static_expected = locals()["expected_static"]()
-    df_transformed = extract_transform_and_apply(df, result[0][2])
-    print(df)
-    print(df_transformed)
-    json_response = {
-        "initial": df.to_json(),
-        "expected": df_transformed.to_json(),
-        "static_example": df_static.to_json(),
-        "expected_static": df_static_expected.to_json(),
-    }
-    return jsonify(response=json_response)
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    backend_port = os.environ["BACKEND_PORT"]
+    if os.environ["ENV"] == "prod":
+        from OpenSSL import SSL
+
+        context = SSL.Context(SSL.TLSv1_2_METHOD)
+        context.use_privatekey_file('/etc/letsencrypt/live/dataframetrainer.com/privkey.pem')
+        context.use_certificate_chain_file('/etc/letsencrypt/live/dataframetrainer.com/fullchain.pem')
+        context.use_certificate_file('/etc/letsencrypt/live/dataframetrainer.com/cert.pem')
+        app.run(host='0.0.0.0', port=backend_port, threaded=True, ssl_context=context)
+    else:
+        app.run(host="0.0.0.0", port=backend_port, threaded=True)
